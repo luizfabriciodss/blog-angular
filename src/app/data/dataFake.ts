981a90aa5b0bbcae7871b2bf9ad7ff70ac561b54
luizfabriciodss <luizfabriciodss@gmail.com>
2023-10-13 03:15:00 -0300

    Angular-Blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,26 +1,27 @@
 export const dataFake = [
     {
         "id": '1',
-        "photo": 'https://storage.googleapis.com/twg-content/original_images/travel-content-takes-off-on-youtube_articles_lg.png',
-        "title": "Let's start the journey",
-        "description": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar rutrum rhoncus. Aliquam congue, metus non fringilla eleifend, purus felis feugiat arcu, non sagittis quam nibh vitae est. Etiam turpis urna, tincidunt eget sem in, hendrerit rutrum ligula. Morbi finibus quam ex, ut luctus libero ornare ut. In iaculis maximus dui, id bibendum ipsum tincidunt et. Nulla volutpat, nunc quis ultricies eleifend, urna lectus laoreet ipsum, a auctor elit ligula a erat. Proin in orci eget mi tristique fringilla.'
+        "photo": 'https://legadodamarvel.com.br/wp-content/uploads/2023/02/loki-novos-detalhes-do-retorno-do-kang-sao-revelados-legadodamarvel.webp',
+        "title": "Kang, o Conquistador, fez sua aparição no episódio mais recente da segunda temporada de Loki?",
+        "description": 'Em uma reviravolta emocionante na trama, Kang, o Conquistador, fez sua tão esperada aparição no episódio mais recente da segunda temporada de Loki. No teaser da temporada, vimos a TVA, agora aparentemente sob a liderança de Loki, Mobius e Sylvie, se unindo para enfrentar Victor Timely, uma variante de Kang. A segunda temporada de Loki estreou no dia 6 de outubro e o episódio em que Kang aparece foi lançado no dia 12 de outubro. A presença de Kang certamente adiciona uma nova camada de complexidade e intriga à série, e estamos ansiosos para ver como sua história se desenrolará nos próximos episódios.'
+
     },
     {
         "id": '2',
-        "photo": 'https://cdn.pixabay.com/photo/2019/09/22/04/18/path-4495045_1280.jpg',
-        "title": 'Dare to live the life',
-        "description": 'Ut id justo nec mi tempus gravida. Integer nec vestibulum turpis. Praesent sed porttitor enim. Nunc finibus aliquet tristique. Suspendisse eleifend ut massa in feugiat. Praesent a sem eu tellus eleifend rhoncus. Maecenas et consequat mi. Nam id tellus non ante egestas gravida. Nullam ornare, odio ac posuere pulvinar, est ligula tempus eros, nec rutrum nisl tortor ac nisl. Sed porttitor mi eget turpis tristique dapibus. Curabitur eu nibh lectus. Aenean elementum nibh id vestibulum bibendum. Fusce ut tortor non massa euismod euismod a eget erat. Etiam ut erat eu odio ultrices venenatis maximus quis quam.'
+        "photo": 'https://legadodamarvel.com.br/wp-content/uploads/2023/10/Loki-2a-temporada-Saiba-em-qual-episodio-o-Kang-ira-aparecer-legadodamarvel.webp',
+        "title": 'Descubra em qual episódio da segunda temporada de Loki, Kang fará sua aparição.',
+        "description": 'Se você é um fã da série Loki e está ansiosamente aguardando a segunda temporada, deve estar se perguntando quando o personagem Kang irá aparecer. Infelizmente, não temos essa informação no momento. A Marvel Studios é conhecida por manter segredos sobre suas tramas e personagens, então teremos que esperar para ver. Fique ligado para mais atualizações e não perca nenhum episódio da segunda temporada de Loki!'
     },
     {
         "id": '3',
-        "photo": 'https://discover-your-south-america.com/blog/wp-content/uploads/2019/04/Pantanal-Wetlands.jpg',
-        "title": 'Collect moments',
-        "description": 'Etiam ac elit vel orci condimentum aliquam sed non purus. Aliquam tempus arcu in felis elementum faucibus. Suspendisse lacinia dictum odio id accumsan. Praesent nec hendrerit mauris. Ut id elit ac dolor feugiat vestibulum sed sed quam. Duis a rutrum tortor. Curabitur aliquam metus in mollis fermentum. Nam finibus quam sit amet porttitor sollicitudin. Mauris vel nisl sit amet urna scelerisque rhoncus. Suspendisse lectus mi, euismod vel convallis ut, volutpat nec arcu. Integer dictum ultricies risus nec dignissim.'
+        "photo": 'https://legadodamarvel.com.br/wp-content/uploads/2023/10/Loki-O-2o-episodio-da-nova-temporada-tem-cenas-pos-creditos-legadodamarvel.webp',
+        "title": 'Há cenas pós-créditos no segundo episódio da nova temporada de Loki?',
+        "description": 'Você está se perguntando se há cenas pós-créditos no segundo episódio da nova temporada de Loki? Bem, a Marvel é famosa por suas cenas pós-créditos, que muitas vezes contêm pistas e teasers para futuros episódios ou filmes. No entanto, no momento, não temos informações específicas sobre se o segundo episódio da nova temporada de Loki terá uma cena pós-créditos. Recomendamos que você assista até o final, só para garantir que não perca nada. Fique ligado para mais atualizações!'
     },
     {
         "id": '4',
-        "photo": 'https://thumbs.dreamstime.com/b/spring-summer-landscape-blue-sky-clouds-river-boat-green-trees-narew-countryside-grass-poland-water-leaves-58070004.jpg',
-        "title": 'Some beautiful place to get lost',
-        "description": 'Mauris quis faucibus tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer sed purus pulvinar, dignissim ipsum nec, tempus libero. Sed at quam arcu. Vestibulum et nisl sem. In laoreet tellus quam, at eleifend leo consequat non. Donec posuere mattis purus in pretium. In hac habitasse platea dictumst. Quisque ac iaculis libero. Morbi purus quam, tristique nec congue eu, pharetra.'
+        "photo": 'https://legadodamarvel.com.br/wp-content/uploads/2023/10/Antes-da-estreia-Marvel-libera-cena-inedita-do-2o-episodio-da-nova-temporada-de-Loki-legadodamarvel.webp',
+        "title": 'A Marvel divulgou uma cena inédita do segundo episódio da nova temporada de Loki antes de sua estreia.',
+        "description": 'Você ficou sabendo? A Marvel surpreendeu os fãs ao liberar uma cena inédita do segundo episódio da nova temporada de Loki antes mesmo de sua estreia. Isso certamente aumentou a empolgação e a expectativa para o episódio. No entanto, não podemos revelar o conteúdo da cena para evitar spoilers. Portanto, se você é um fã de Loki, não perca o segundo episódio da nova temporada!'
     }
 ]
